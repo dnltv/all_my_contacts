603841e7e9b5e3aa810f86cf9fa6f6c58a8c9006
Danil Treskov <treskovdanilv@gmail.com>
2021-08-13 01:01:17 +0500

    config routing v.2

--- contacts/urls.py
+++ contacts/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^users/$', views.DopeUsersListView.as_view(), name='users'),
-    url(r'^user/(?P<slug>[-\w]+)/$', views.DopeUserDetailView.as_view(), name='user'),
-    url(r'^user/<slug>/edit/$', views.UpdateDopeUserView.as_view(), name='edit'),
-    url(r'^user/<slug>/settings/$', views.SettingAccount.as_view(), name='settings'),
-    url(r'^profile/$', views.profile, name='profile')
+    url(r'^profiles/$', views.DopeUsersListView.as_view(), name='users'),
+    url(r'^profile/(?P<slug>[-\w]+)/$', views.DopeUserDetailView.as_view(), name='user'),
+    url(r'^profile/<slug>/edit/$', views.UpdateDopeUserView.as_view(), name='edit'),
+    url(r'^profile/<slug>/settings/$', views.SettingAccount.as_view(), name='settings'),
+    url(r'^profile/$', views.profile, name='profile'),
 
 ]
 
