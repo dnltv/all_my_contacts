c31e1500e9bd1bb29c74ced6d7ff0f3c573f3b2f
Danil Treskov <treskovdanilv@gmail.com>
2021-08-18 22:48:39 +0500

    base template connect to bootstrap, modify CustomUserChangeForm

--- contacts/forms.py
+++ contacts/forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser, DopeUser
-from django.forms import ModelForm
+from django.forms import ModelForm, EmailInput, TextInput
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -15,6 +15,16 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('email', 'first_name')
+        widgets = {
+            'email': EmailInput(attrs={
+               'class': 'form-control',
+                'placeholder': 'Введите email',
+            }),
+            'first_name': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введите имя'
+            }),
+        }
 
 
 class EditDopeUserModelForm(ModelForm):
